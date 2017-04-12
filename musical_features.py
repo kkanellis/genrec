306007b4d28d16506135b1f027db9057b951d0c0
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-04-12 15:57:45 +0300

    features: replaced custom code with librosa library

--- musical_features.py
+++ musical_features.py
@@ -1,54 +1,85 @@
+import librosa
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class TextureWindow:
 
-    def __init__(self, windows, prev_windows, fft_length=512):
-        self.windows = windows
-        self.prev_windows = prev_windows
-        self.fft_length = fft_length // 2 + 1
+    def __init__(self, tex_wnd, prev_tex_wnd, fft_len=512, sr=22050):
+        # split texture (prev_)window to analysis windows
+        self.tex_wnd = tex_wnd
+        self.tex_wnds = self._split_tex_wnd(tex_wnd, fft_len)
+        self.prev_tex_wnds = self._split_tex_wnd(prev_tex_wnd, fft_len)
 
-        self.fft_mags = np.array([
-            np.absolute( np.fft.rfft(window) )
-                for window in self.windows
-        ])
-        self.prev_fft_mags = np.array([
-            np.absolute( np.fft.rfft(window) )
-                for window in self.prev_windows
-        ])
+        self.an_wnd_len = fft_len
+        self.fft_len = fft_len // 2 + 1
+        self.sr = sr
 
-        #print( self._get_wnd_nrg( self.fft_mags[0]) )
-        #q = np.array(windows[0], dtype=np.int64)
-        #print( np.sum( q ** 2 ) )
+        #plt.magnitude_spectrum(np.array(windows).ravel(), Fs=sampling_rate)
+        #plt.show()
+
+
+        """
+        self._fft_tex_wnds = np.array([
+            np.abs(librosa.stft(
+                y=wnd,
+                n_fft=fft_len,
+                hop_length=fft_len+1,
+            )).ravel()
+            for wnd in self.tex_wnds
+        ])
+        #print(self._fft_tex_wnds)
+
+        """
+        # TODO: check FFT coefficients correctness
+        self.fft_tex_wnds = np.abs(
+            librosa.stft(
+                y=tex_wnd,
+                n_fft=fft_len,
+                hop_length=fft_len,
+            )
+        )
+        """
+        print(self.fft_tex_wnds.T)
+        print( (self._fft_tex_wnds == self.fft_tex_wnds.T) )
+        plt.figure(1)
+        plt.plot(self.fft_tex_wnds)
+        plt.show()
+
+        self.fft_prev_fft_wnds = np.array([
+            np.abs( librosa.stft(
+                y=wnd,
+                n_fft=fft_len,
+                hop_length=fft_len+1,
+            )).ravel()
+            for wnd in self.prev_tex_wnds
+        ])
+        """
+
+        self.prev_fft_wnds = np.abs(
+            librosa.stft(
+                y=prev_tex_wnd,
+                n_fft=fft_len,
+                hop_length=fft_len,
+            )
+        )
+        #print(len(self.prev_fft_wnds[0]))
 
     def centroid(self):
-        f_arr = np.array([ f for f in range(self.fft_length) ])
-
-        centroids = [ ]
-        for fft_mag in self.fft_mags:
-            centroids.append(
-                np.dot(fft_mag, f_arr) / np.sum(fft_mag)
-            )
-
+        centroids = librosa.feature.spectral_centroid(
+            S=self.fft_tex_wnds
+        ).ravel()
         return np.mean(centroids), np.std(centroids)
 
     def rolloff(self):
-        rolloffs = [ ]
-        for i in range( len(self.fft_mags) ):
-            roff = 0.85 * np.sum(self.fft_mags[i])
-
-            # TODO: remove loop
-            psum = 0
-            for R in range(self.fft_length):
-                if psum + self.fft_mags[i][R] > roff:
-                    break
-                psum += self.fft_mags[i][R]
-
-            rolloffs.append(R)
-
+        rolloffs = librosa.feature.spectral_rolloff(
+            S=self.fft_tex_wnds
+        ).ravel()
         return np.mean(rolloffs), np.std(rolloffs)
 
 
     def flux(self):
+        """
         mags_norm = np.array([
             fft_mag / self._get_wnd_nrg(fft_mag)
             for fft_mag in self.fft_mags
@@ -64,31 +95,25 @@ class TextureWindow:
             for prev, curr in
                 zip(prev_mags_norm, mags_norm)
         ]
+        """
 
+        fluxes = [0, 0]
         return np.mean(fluxes), np.std(fluxes)
 
-
-        #print( np.sum(self.fft_mags[0]) )
-        #print( self._get_wnd_nrg( self.fft_mags[0] ) )
-
-
     def zero_crossings(self):
-        zc = [ ]
-        for w in self.windows:
-            zc.append(
-                np.sum([
-                    (w[i-1] < 0) != (w[i] < 0)
-                    for i in range(1, len(w))
-                ])
-            )
-
+        zc = librosa.feature.zero_crossing_rate(
+            y=self.tex_wnd,
+            frame_length=self.an_wnd_len,
+            hop_length=self.an_wnd_len,
+        ).ravel()
         return np.mean(zc), np.std(zc)
 
     def low_energy(self):
-        w_nrg = np.array([
-            self._get_wnd_nrg(fft_w)
-            for fft_w in self.fft_mags
-        ])
+        w_nrg = librosa.feature.rmse(
+            S=self.fft_tex_wnds,
+            frame_length=self.an_wnd_len,
+            hop_length=self.an_wnd_len
+        ).ravel()
         avg_nrg = np.mean(w_nrg)
 
         return np.sum(
@@ -101,6 +126,22 @@ class TextureWindow:
         using FFT magnitude (Parseval's theorem)
         """
         return np.sum( (fft_w ** 2) / self.fft_length )
+
+    def _split_tex_wnd(self, tex_wnd, chunk_sz):
+        wnds = [ ]
+        for i in range(0, len(tex_wnd) - 1, chunk_sz):
+            wnds.append(
+                tex_wnd[i:i+chunk_sz]
+            )
+
+        # last wnd is smaller
+        if len(wnds[-1]) < chunk_sz:
+            wnds[-1] = np.array(
+                list(wnds[-1]) + (chunk_sz - len(wnds[-1])) * [0.0]
+            )
+
+        return wnds
+
 
     def fv(self):
         mean_centroid, std_centroid = self.centroid()
