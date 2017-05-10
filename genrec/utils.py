a5d55d64e26d35ab901ab13822669c97b01e66c5
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-05-10 23:14:51 +0300

    utils: implement object JSONEncoder
    Object must implement 'to_json' method

--- genrec/utils.py
+++ genrec/utils.py
@@ -1,3 +1,14 @@
+from json import JSONEncoder
+
+
+class JSONEncoderObj(JSONEncoder):
+    def default(self, obj):
+        try:
+            return obj.to_json()
+        except:
+            pass
+        return self.default(obj)
+
 
 def chunks(l, chunk_size):
     for i in range(0, len(l), chunk_size):
