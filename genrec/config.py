3dfcae83ccec13a320de51aae95bc22bf2ac031c
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-05-10 23:15:31 +0300

    config: add configuration file

---
+++ genrec/config.py
@@ -0,0 +1,8 @@
+# Test filename
+FILENAME = '/home/sfi/Programming/genre-classification/db/gtzan/jazz/jazz.00000.au'
+
+# Absolute path to GTZAN db
+GTZAN_DIR = '/home/sfi/Programming/genre-classification/db/gtzan/'
+
+# Genre list
+GENRES = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock' ]
