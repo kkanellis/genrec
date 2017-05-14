ee14687ef80b474b037005a919b59dff87af058f
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-05-14 17:55:08 +0300

    classifier: add metrics (train/test score & confusion matrices)

--- genrec/classifier.py
+++ genrec/classifier.py
@@ -3,6 +3,7 @@ from copy import copy
 import numpy as np
 
 from sklearn.externals import joblib
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -10,7 +11,6 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
-
 
 from genrec.logger import get_logger
 
@@ -21,10 +21,10 @@ class MusicGenreClassifier:
 
         self.logger = get_logger('classifier')
 
+        self.scaler = None
         if not clf_kwargs:
             clf_kwargs = { }
 
-        self.req_scaling = False
         if type == 'knn':
             self.proto_clf = KNeighborsClassifier(**clf_kwargs)
         elif type == 'svm':
@@ -35,36 +35,58 @@ class MusicGenreClassifier:
             self.proto_clf = GaussianNB(**clf_kwargs)
         elif type == 'mlp':
             self.proto_clf = MLPClassifier(**clf_kwargs)
-            self.req_scaling = True
+            self.scaler = StandardScaler()
         else:
             raise LookupError('Classifier type "{}" is invalid'.format(type))
 
         self._convert_data(data)
         self.randstate = np.random.RandomState()
 
-    def kfold_test(self, k=10, times=1000):
-        accuracy = np.zeros(times)
+    def kfold_test(self, k=10, iters=100):
+        iters = iters // k if iters % k == 0 else iters // k + 1
+        n_genres = len(self.genres)
 
-        i = 0
-        for _ in range(times // k):
+        train_acc = np.zeros(iters * k)
+        test_acc = np.zeros(iters * k)
+        cms = np.zeros((iters, n_genres, n_genres))
+
+        clf = copy(self.proto_clf)
+        for iter in range(iters):
             kf = KFold(n_splits=k, random_state=self.randstate, shuffle=True)
+            cm = np.zeros((10, 10))
 
-            for train_idx, test_idx in kf.split(self.X, self.y):
+            for i, (train_idx, test_idx) in enumerate(kf.split(self.X, self.y)):
                 X_train, X_test = self.X[train_idx], self.X[test_idx]
                 y_train, y_test = self.y[train_idx], self.y[test_idx]
 
-                if self.req_scaling:
-                    X_train, X_test = self._scale_data(X_train, X_test)
+                if self.scaler:
+                    self.scaler.fit(X_train)
+                    X_train, X_test = self.scaler.transform(X_train), \
+                                        self.scaler.transform(X_test)
 
-                clf = copy(self.proto_clf)
                 clf.fit(X_train, y_train)
 
-                accuracy[i] = clf.score(X_test, y_test)
-                i += 1
+                idx = iter * iters + i
+                train_acc[idx] = clf.score(X_train, y_train)
+                test_acc[idx] = clf.score(X_test, y_test)
 
-        self.logger.info('Mean accuracy: {:5.2f}% +- ({:5.2f}%)'.format(
-            np.mean(accuracy) * 100.0, np.std(accuracy) * 100.0
+                y_pred = clf.predict(X_test)
+                cm += confusion_matrix(y_test, y_pred)
+
+            cms[iter,:,:] = cm
+
+        # Calculate avg confusion matrix
+        mean_cm = np.mean(cms, axis=0)
+
+        # Print metrics
+        self.logger.info('Train accuracy: {:5.2f}% +- ({:5.2f}%)'.format(
+            np.mean(train_acc) * 100.0, np.std(train_acc) * 100.0
         ))
+        self.logger.info('Test accuracy: {:5.2f}% +- ({:5.2f}%)'.format(
+            np.mean(test_acc) * 100.0, np.std(test_acc) * 100.0
+        ))
+        self.logger.info('Genres: {}'.format(self.genres))
+        self.logger.info('Confusion matrix: \n{}'.format(mean_cm))
 
     def load(self, filepath):
         self.proto_clf = joblib.load(filepath)
@@ -73,7 +95,7 @@ class MusicGenreClassifier:
         joblib.sump(self.proto_clf, filepath)
 
     def _convert_data(self, data):
-
+        """ Converts data from dataset to sklearn format """
         X, y = [ ], [ ]
         for genre, aufiles in data.items():
             for aufile in aufiles:
@@ -82,9 +104,4 @@ class MusicGenreClassifier:
 
         self.X = np.array(X)
         self.y = np.array(y)
-
-    def _scale_data(self, X_train, X_test):
-        scaler = StandardScaler()
-        scaler.fit(X_train)
-        return scaler.transform(X_train), scaler.transform(X_test)
 
