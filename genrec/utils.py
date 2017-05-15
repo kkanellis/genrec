745c5f008f8949831ea267cb864745494b02f739
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-05-15 19:04:37 +0300

    utils: numpy formatting context manager

--- genrec/utils.py
+++ genrec/utils.py
@@ -1,5 +1,7 @@
 from json import JSONEncoder
+import contextlib
 
+import numpy as np
 
 class JSONEncoderObj(JSONEncoder):
     def default(self, obj):
@@ -9,6 +11,15 @@ class JSONEncoderObj(JSONEncoder):
             pass
         return self.default(obj)
 
+
+@contextlib.contextmanager
+def np_printoptions(*args, **kwargs):
+    original = np.get_printoptions()
+    np.set_printoptions(*args, **kwargs)
+    try:
+        yield
+    finally:
+        np.set_printoptions(**original)
 
 def chunks(l, chunk_size):
     for i in range(0, len(l), chunk_size):
