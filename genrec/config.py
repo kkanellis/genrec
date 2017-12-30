2f3fe068a9a45b5149984ff5345fa28ea1f6ab3c
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-12-30 18:09:29 +0200

    Change absolute paths to relative ones

--- genrec/config.py
+++ genrec/config.py
@@ -1,8 +1,8 @@
 # Absolute path to GTZAN db
-GTZAN_DIR = '/home/sfi/Programming/genrec/db/gtzan/'
+GTZAN_DIR = '../db/gtzan/'
 
 # Absolute path to directory to save the plots
-PLOTS_DIR = '/home/sfi/Programming/genrec/plots/'
+PLOTS_DIR = '../plots/'
 
 # Genre list
 GENRES = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock' ]
