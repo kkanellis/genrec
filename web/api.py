787183f7586b139a00bfc4fb1b1bdb37a23212e4
Konstantinos Kanellis <kkanelli@hotmail.com>
2018-01-01 20:06:20 +0200

    Web API barebones

---
+++ web/api.py
@@ -0,0 +1,15 @@
+from web.classifiers import get_dataset_models
+
+class GenrecAPI:
+    def __init__(self):
+        pass
+
+    def get_available_datasets(self):
+        pass
+
+    def get_available_classifiers(self, dataset):
+        pass
+
+    def predict_song(self, filepath):
+        pass
+
