49077010065e865f90c15178d0c09225c8062f89
Eleftherios P. Loukas <eleftheriosloukas@gmail.com>
2018-01-06 20:15:23 +0200

    Get datasets(folders) as a list

--- web/api.py
+++ web/api.py
@@ -1,4 +1,5 @@
-from web.classifiers import get_dataset_models
+from web.classifiers import get_dataset_models, _get_valid_dirs
+import os.path
 
 class GenrecAPI:
     def __init__(self):
@@ -13,9 +14,21 @@ class GenrecAPI:
         Args:
             self: object of the GenrecAPI Class
         Returns:
-            available datasets
+            available datasets to be chosen as a training set
         """
+        exclude_prefixes = ('__', '.')  # Exclusion prefixes for hidden subdirs
+        for datasetPath, _ in get_dataset_models(): # Find path of datasets
+            pass
 
+        exclude_prefixes = ('__', '.')  # exclusion prefixes
+        for _, dirNames, _ in os.walk(datasetPath):
+            # Exclude all dirs starting with exclude_prefixes
+            dirNames[:] = [dirName
+                       for dirName in dirNames
+                       if not dirName.startswith(exclude_prefixes)]
+            break
+
+        print(dirNames)
         pass
 
     def get_available_classifiers(self, dataset):
@@ -23,3 +36,11 @@ class GenrecAPI:
 
     def predict_song(self, filepath):
         pass
+
+def main():
+
+    testObject = GenrecAPI()
+    testObject.get_available_datasets()
+    pass
+
+main()
