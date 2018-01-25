6342c8fe0014026c1dfea393876701230ebb3baa
Konstantinos Kanellis <kkanelli@hotmail.com>
2018-01-26 01:52:28 +0200

    web: Download video only if not exist already

--- web/server/ydl/downloader.py
+++ web/server/ydl/downloader.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
+import os
+import os.path
+
 import youtube_dl
 
-
 class DownloaderAPI():
+    DOWNLOAD_LOCATION = 'downloads/'
+    available_videos = { }
 
-    def __init__(self, url):
+    def __init__(self):
         self.ydl_opts = { # options for youtube_dl
             'format': 'bestaudio/best',
             'postprocessors': [{
@@ -12,27 +16,44 @@ class DownloaderAPI():
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }],
-            'outtmpl': 'downloads/%(title)s.%(ext)s'
+            'outtmpl': self.DOWNLOAD_LOCATION + '%(id)s.%(ext)s'
         }
-
-        # youtube link
-        self.url = url
 
         # initiliaze the ydlObject
         self.ydl = youtube_dl.YoutubeDL(self.ydl_opts)
+        self._populate_entries()
 
-
-    def urlValidation(self):
+    def is_url_valid(self, url):
         try:
             # Get information about the YouTube video/song
-            info_dict = self.ydl.extract_info(self.url, download=False)
+            info = self.ydl.extract_info(url, download=False)
+            return info['id']
+        except:
+            # The url does not exists or is wrong!
+            return None
 
-        except: # The url does not exists or is wrong!
-            raise 
+    def download(self, videoId, url):
+        if videoId not in self.available_videos:
+            try:
+                self.ydl.download([url])
+            except: # something went wrong at downloading process!
+                raise
 
-    def download(self):
-        try:
-            self.ydl.download([self.url])
+            filepath = os.path.join( self.DOWNLOAD_LOCATION, videoId + '.mp3' )
+            self.available_videos[videoId] = filepath
 
-        except: # something went wrong at downloading process!
-            raise
+            print(f'Saved video "{videoId}" @ "{filepath}"')
+        else:
+            print(f'Video "{videoId}" already downloaded!')
+
+    def get_filepath(self, videoId):
+        return self.available_videos[videoId]
+
+    def _populate_entries(self):
+        entries = { }
+        for p in os.scandir(self.DOWNLOAD_LOCATION):
+            if p.is_file():
+                videoId = p.name.split('.')[0]
+                entries[videoId] = os.path.realpath(p.path)
+
+        self.available_videos = entries
